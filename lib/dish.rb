fead7f17672c9bb93b669ab2b4e7b26630226d97
Louise <louiselai88@gmail.com>
2014-04-26 23:54:39 +0100

    major changes

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,10 +1,28 @@
 class Dish
 
-	def initialize(price)
+	def initialize(dish_name, price)
+		@dish_name = dish_name
 		@price = price
+		@line_item = []
+		@line_item << {"#{dish_name}".to_sym => "#{price}"}
+		# @menu = [{curry: 8}]
+	end
+
+	def dish_name
+		@dish_name
 	end
 
 	def price
 		@price
 	end
+
+	def line_item
+		@line_item
+	end
+
+	def price
+		self.line_item[0][@dish_name.to_sym].to_i #need to refer to index
+	end
+
+
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,12 +2,15 @@ require "dish"
 
 describe "dish" do
 
-		it "can be initialized with name and price" do
-			curry = Dish.new(8)
-			expect(curry.price).to eq 8
+		let (:dish_one) {Dish.new("curry", 8)}
+
+		it "is put into a menu upon initialization" do
+
+			expect(dish_one.line_item.count).to eq 1
 		end
 
-
-
+		it "has a price" do
+			expect(dish_one.price).to eq 8
+		end
 
 end
