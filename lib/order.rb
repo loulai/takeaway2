34bc08ebe7ca3cc9fa670f2356aa06416409566f
Louise <louiselai88@gmail.com>
2014-04-28 00:43:06 +0100

    adding text sending ability

--- lib/order.rb
+++ lib/order.rb
@@ -6,11 +6,17 @@ class Order
 
 	attr_reader :list_of_line_items
 
+	attr_reader :estimated_total_price
+
 	def correct_total_price
 		# list_of_line_items = [my_line_item, another_line_item]
 		@list_order_prices = list_of_line_items.map {|individual_line_item| individual_line_item.line_price }
-		#                    => [15, 4]
+		# => [15, 4]
 		@list_order_prices.inject(:+)
+	end
+
+	def place_order
+		raise "you cannot math" if estimated_total_price != correct_total_price
 	end
 
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,5 +1,6 @@
 require "order"
 require "dish"
+require "send_sms"
 
 
 describe "Order" do
@@ -10,7 +11,7 @@ describe "Order" do
 	let (:my_line_item) {LineItem.new(burrito, 3)}
 	let (:another_line_item) {LineItem.new(pie)}
 
-	let (:my_order) {Order.new([my_line_item, another_line_item], 19)}
+	let (:my_order) {Order.new([my_line_item, another_line_item], 18)}
 	
 	context "upon creation" do
 
@@ -26,8 +27,12 @@ describe "Order" do
 
 	context "ordering" do
 		
-		it "will raise an error when wrong order price is given" do
-		expect(my_order.place_order).to raise_error(RuntimeError)
+		# it "will raise an error when estimated total price is wrong" do
+		# expect(my_order.place_order).to raise_error(RuntimeError)
+		# end
+
+		it "will send a text message when given the correct price" do
+			expect
 		end
 
 	end
