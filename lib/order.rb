16ff19c4a186ac6c2458fad0d628c3d62c784f83
Louise <louiselai88@gmail.com>
2014-04-27 22:57:22 +0100

    first test for Oder class passes

---
+++ lib/order.rb
@@ -0,0 +1,13 @@
+class Order
+
+	def initialize(list_of_line_items, estimated_total_price)
+		@list_of_line_items = list_of_line_items
+	end
+
+	attr_reader :list_of_line_items
+
+
+
+
+
+end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,16 +1,18 @@
 require "order"
+require "dish"
 
 
 describe "Order" do
 
+	let (:burrito) {Dish.new("burrito", 5)}
 	let (:pie) {Dish.new("pie", 4)}
 
 	let (:my_line_item) {LineItem.new(burrito, 3)}
 	let (:another_line_item) {LineItem.new(pie)}
 	
-	it "has a LineItem as one of its components" do
-		order = Order.new(line_item, 15) #forced est for now
-		expect(order.what_line_items).to eq my_line_item
+	it "has an array of LineItems as one of its components" do
+		order = Order.new([my_line_item], 15) #forced est for now
+		expect(order.list_of_line_items).to eq [my_line_item]
 	end
 
 end
