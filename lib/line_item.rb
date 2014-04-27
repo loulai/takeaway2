9904768a539332308467384945bcab3a3188cfcb
Louise <louiselai88@gmail.com>
2014-04-27 22:16:56 +0100

    LineItem passes first rspec test

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -1,2 +1,14 @@
 class LineItem
+
+	def initialize(dish, quantity)
+		@dish = dish
+		@quantity = quantity
+	end
+
+	def dish_name
+		@dish.name
+	end
+
+
+
 end
--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -1,6 +1,9 @@
 require "line_item"
 
+
 describe "line_item" do
+
+	let (:burrito) {Dish.new("burrito", 5)}
 	
 	it "when created, has a Dish as one of its components" do
 		expect(LineItem.new(burrito, 3).dish_name).to eq "burrito"
