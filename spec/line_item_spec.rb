4117291be99fe2c3d5a80e7be7d09bc91471a63f
Louise <louiselai88@gmail.com>
2014-04-27 22:30:58 +0100

    fixed indentation

--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -7,17 +7,17 @@ describe "line_item" do
 
 	context "when created" do
 	
-	it "has a Dish as one of its components" do
-		expect(LineItem.new(burrito, 3).dish_name).to eq "burrito"
-	end
+		it "has a Dish as one of its components" do
+			expect(LineItem.new(burrito, 3).dish_name).to eq "burrito"
+		end
 
-	it "has a quantity as one of its components" do
-		expect(LineItem.new(burrito, 3).quantity).to eq 3
-	end
+		it "has a quantity as one of its components" do
+			expect(LineItem.new(burrito, 3).quantity).to eq 3
+		end
 
-	it "when not specified, a line item has a default quantity of one" do
-		expect(LineItem.new(burrito).quantity).to eq 1
-	end
+		it "when not specified, a line item has a default quantity of one" do
+			expect(LineItem.new(burrito).quantity).to eq 1
+		end
 
 	end
 
