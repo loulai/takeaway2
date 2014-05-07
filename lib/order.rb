44be9212b22b609432049473922467cabe8296a8
Louise <louiselai88@gmail.com>
2014-05-07 12:05:30 +0100

    refactored, removed unnecessary @estimate_status, which made the code wordy

--- lib/order.rb
+++ lib/order.rb
@@ -1,41 +1,29 @@
 
 class Order
 
-	def initialize(list_of_line_items, estimated_total_price)
-		@list_of_line_items = list_of_line_items
-		@estimated_total_price = estimated_total_price
-		is_price_estimate_correct?
-	end
-
 	attr_reader :list_of_line_items
 
 	attr_reader :estimated_total_price
 
+	def initialize(list_of_line_items, estimated_total_price)
+		@list_of_line_items = list_of_line_items
+		@estimated_total_price = estimated_total_price
+	end
+	
 	def correct_total_price
-		# list_of_line_items = [my_line_item, another_line_item]
 		@list_order_prices   = list_of_line_items.map {|individual_line_item| individual_line_item.line_price }
-		# => [15, 4]
 		@correct_total_price = @list_order_prices.inject(:+)
 		@correct_total_price
 	end
 
 	def is_price_estimate_correct?
-		if correct_total_price == estimated_total_price
-			@estimate_status = true
-			true
-		else 
-			 @estimate_status = false
-			 false
-		end
+		correct_total_price == estimated_total_price
 	end
 
 	def place_order
-		if @estimate_status == false
-			raise "You guessed the price wrong!"
-
-		else
-			send_text("Thank you dear customer, your order will arrive in #{order_time}")
-		end
+		raise "You guessed the price wrong!" unless is_price_estimate_correct?
+	
+		send_text("Thank you dear customer, your order will arrive in #{order_time}")	
 	end
 
 	def order_time
